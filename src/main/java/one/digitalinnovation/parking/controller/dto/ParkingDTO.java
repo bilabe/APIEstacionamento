c86579ed744333370091a68fe78be1a047ec328a
Eliab Araujo <eliab2100@gmail.com>
2022-11-10 11:36:19 -0300

    Lombok

---
+++ src/main/java/one/digitalinnovation/parking/controller/dto/ParkingDTO.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.parking.controller.dto;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ParkingDTO {
+
+    private String id;
+    private String license;
+    private String state;
+    private String model;
+    private String color;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime entryDate;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime exitDate;
+    private Double bill;
+}
